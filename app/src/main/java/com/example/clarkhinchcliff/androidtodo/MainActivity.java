8efee0d6be900f26f924d965c7ccaa0d2758a283
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-06-26 16:38:23 -0700

    Created todo list -next is to connect text field and submit button

--- app/src/main/java/com/example/clarkhinchcliff/androidtodo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/androidtodo/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.clarkhinchcliff.androidtodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> toDoAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(toDoAdapter);
+    }
+
+    public void populateArrayItems() {
+        todoItems = new ArrayList<String>();
+        todoItems.add("Eat a house");
+        todoItems.add("Build the pizza");
+        todoItems.add("Paint the application");
+        toDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
     }
 }
